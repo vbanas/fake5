832c9ee12df56252bf24cdf34c2dd4e6ab5cfe69
theihor <the.ihor@gmail.com>
2016-08-06 00:48:46 +0300

    fixed

--- src/simple-state.lisp
+++ src/simple-state.lisp
@@ -1,17 +1,15 @@
 (defpackage :src/simple-state
-  (:use :common-lisp
-<<<<<<< HEAD
+  (:use :common-lisp 
         :2d-geometry
         :src/drawer
         :src/polygons)
-  (:import-from :2d-geometry
-=======
-        :cl-geometry
-        :src/utils
-        :src/drawer
-        :src/mcts)
+  (:import-from :2d-geometry 
+                :cl-geometry
+                :src/utils
+                :src/drawer
+                :src/polygons
+                :src/mcts)
   (:import-from :cl-geometry
->>>>>>> 53a7be8d2fa22af2f705588edb35131bbe244f9a
                 :point-equal-p))
 
 (in-package :src/simple-state)
