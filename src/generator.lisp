c75347118b97d05438745348d1a3e853d9f69cc3
theihor <the.ihor@gmail.com>
2016-08-07 14:35:35 +0300

    added rotation to generator

--- src/generator.lisp
+++ src/generator.lisp
@@ -3,18 +3,22 @@
         :cl-geometry
         :src/drawer
 	:src/simple-state
-        :src/printer))
+    :src/printer
+    :src/matrix))
 
 (in-package :src/generator)
 
 (defun generator-test (file &key res-file)
   (let* ((new-specs)
-	 (specs-size 11))
+         (specs-size 11)
+         (m-for-rotation (1+ (random 10000)))
+         (n-for-rotation (1+ (random 10000))))
     (loop for n from 1 to specs-size do
-	 (push (list :a (+ (random 3) 1/100) :b (+ (random 5) 1/100) :c (- (/ (random 7) (1+ (random 11)))) :x (random 7) :y (random 5)) new-specs))
+         (push (list :a (+ (random 3) 1/100) :b (+ (random 5) 1/100) :c (- (/ (random 7) (1+ (random 11)))) :x (random 7) :y (random 5)) new-specs))
     (labels ((%do ()
                (print-solution
-                (src/simple-state::fold-quad-and-show file new-specs :animate t))))
+                (mapcar (lambda (p) (rotate-polygon p m-for-rotation n-for-rotation))
+                        (src/simple-state::fold-quad-and-show file new-specs :animate t)))))
       (if res-file
           (with-open-file (*standard-output* res-file :direction :output
                                              :if-exists :supersede
